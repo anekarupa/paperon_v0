17c6013df93c90c2d5ed7d47d69be8251be50de3
anekarupa <anekarupa26@gmail.com>
2020-11-30 23:36:50 +0700

    add browser router, switch and route

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import theme from './Theme'
+import FirstPage from './pages/FirstPage';
 
 export default function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
-      <React.Fragment>
-
-      </React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={FirstPage} />
+        </Switch>
+      </BrowserRouter>
     </MuiThemeProvider>
   );
 }
